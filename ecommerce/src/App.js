f9a1f838369d68298af53deb2ff4486bf81f211b
jonathan bola <bolajonathan@yahoo.com>
2023-05-02 12:18:58 -0500

    Navbar

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { CartContext } from "./pages/ProductPage";
+
 
 function App() {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItem([...cartItem, item]);
+  };
+
+  // local storage
+  useEffect(() => {
+    const json = localStorage.getItem("cartItem");
+    const savedCart = JSON.parse(json);
+    if (savedCart) {
+      setCartItem(savedCart);
+    }
+  }, []);
+
+  useEffect(() => {
+    const json = JSON.stringify(cartItem);
+    localStorage.setItem("cartItem", json);
+  }, [cartItem]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartContext.Provider value={{ cartItem, addToCart, setCartItem }}>
+      
+        Hello 
+        <Navbar/>
+        <Routes>
+          <Route/>
+          <Route/>
+          <Route/>
+          <Route/>
+        </Routes>
+      
+    </CartContext.Provider>
   );
 }
 
